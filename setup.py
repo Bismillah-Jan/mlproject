9ca250c8d32427555466e790926c8c8495cf78e8
Bismillah-Jan <bismillahjan222@gmail.com>
2025-11-18 22:18:39 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements= [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+   # return [req.strip() for req in requirements if req.strip()]
+setup(
+    name="mlproject",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+    author="Bismillah Jan",
+    description="A machine learning project setup", 
+    license="MIT"
+   
+
+)
